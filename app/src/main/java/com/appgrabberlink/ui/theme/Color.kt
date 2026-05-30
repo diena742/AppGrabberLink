fea27a3037f0b72e38104bc78fdcc7a013e1a72d
rsmediazero <60850241+diena742@users.noreply.github.com>
2026-05-30 17:14:36 +0700

    [VOID-UPLOAD] 20260530_171435

---
+++ app/src/main/java/com/appgrabberlink/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.appgrabberlink.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Cyan = Color(0xFF00BCD4)
+val CyanDark = Color(0xFF0097A7)
+val CyanLight = Color(0xFF4DD0E1)
+
+val DarkBackground = Color(0xFF141218)
+val DarkSurface = Color(0xFF1C1B1F)
+val DarkSurfaceVariant = Color(0xFF2B2930)
+val DarkOnBackground = Color(0xFFE6E1E5)
+val DarkOnSurface = Color(0xFFE6E1E5)
+val DarkOnSurfaceVariant = Color(0xFFCAC4D0)
+val DarkOutline = Color(0xFF938F99)
