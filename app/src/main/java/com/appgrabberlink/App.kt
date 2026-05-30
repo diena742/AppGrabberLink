3daf96415116c19eda9e6f898e540ff9bdbe0461
rsmediazero <60850241+diena742@users.noreply.github.com>
2026-05-30 17:13:30 +0700

    [VOID-UPLOAD] 20260530_171328

---
+++ app/src/main/java/com/appgrabberlink/App.kt
@@ -0,0 +1,50 @@
+package com.appgrabberlink
+
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+
+class App : Application() {
+
+    companion object {
+        const val CHANNEL_CAPTURE = \"capture_service\"
+        const val CHANNEL_FLOATING = \"floating_service\"
+        const val CHANNEL_DOWNLOAD = \"download_service\"
+        lateinit var instance: App
+            private set
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+        createNotificationChannels()
+    }
+
+    private fun createNotificationChannels() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val manager = getSystemService(NotificationManager::class.java)
+
+            listOf(
+                NotificationChannel(
+                    CHANNEL_CAPTURE,
+                    \"Capture Service\",
+                    NotificationManager.IMPORTANCE_LOW
+                ),
+                NotificationChannel(
+                    CHANNEL_FLOATING,
+                    \"Floating Panel\",
+                    NotificationManager.IMPORTANCE_LOW
+                ),
+                NotificationChannel(
+                    CHANNEL_DOWNLOAD,
+                    \"Download Service\",
+                    NotificationManager.IMPORTANCE_LOW
+                )
+            ).forEach { channel ->
+                channel.setShowBadge(false)
+                manager.createNotificationChannel(channel)
+            }
+        }
+    }
+}
